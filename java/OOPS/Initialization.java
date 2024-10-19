79a94059850a4141eb173afc96106b9e39375a14
Delta Student <125783753+nischithnayak@users.noreply.github.com>
2024-10-19 10:51:32 +0530

    new

---
+++ java/OOPS/Initialization.java
@@ -0,0 +1,14 @@
+public class Initialization{
+    public static void main(String []args){
+        Student s1=new Student("Nischith Nayak");
+        System.out.println(s1.name);
+        
+    }
+}
+class Student{
+    int roll;
+    String name;
+    Student(String name){
+       this.name=name;
+    }
+}
