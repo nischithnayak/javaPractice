f48524f2fca33c6c37012480d2208305ade089dc
Delta Student <125783753+nischithnayak@users.noreply.github.com>
2024-10-19 10:53:34 +0530

    new

--- java/OOPS/Calling.java
+++ java/OOPS/Calling.java
@@ -1,3 +1,12 @@
 public class Calling {
-    
+    public static void main(String[] args) {
+     Student s1=new Student();   
+    }
+}
+class Student{
+    int roll;
+    String name;
+    Student(){
+        System.out.println("Constructor is called...");
+    }
 }
