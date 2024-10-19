bbe3d2fd316a90924f62e3a701302d3c55c72f83
Delta Student <125783753+nischithnayak@users.noreply.github.com>
2024-10-19 10:44:45 +0530

    update

---
+++ java/OOPS/Default.java
@@ -0,0 +1,10 @@
+//Default Constructor
+public class Default{
+    public static void main(String[] args) {
+        Student s1=new Student();
+    }
+}
+class Student {
+    int rollno;
+    String name;
+}
