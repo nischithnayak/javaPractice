12c54320676acca723f728ca5765d4bab00e6f50
Delta Student <125783753+nischithnayak@users.noreply.github.com>
2024-12-01 15:08:46 +0530

    new

--- java/Arraylist/Syntax.java
+++ java/Arraylist/Syntax.java
@@ -2,19 +2,17 @@ import java.util.ArrayList;
 public class Syntax {
     public static void main(String[] args) {
         ArrayList<Integer> list=new ArrayList<>();
-        list.add(1); //O(1)
-        list.add(2);
-        list.add(3);
-        list.add(4);
+        list.add(1); 
+        list.add(-2);
+        list.add(8);
+        list.add(4); 
         list.add(5);
-        System.out.println(list);
-        System.out.println(list.get(2)); //O(1)
-        list.remove(2); //O(n)
-        System.out.println(list);
-        list.set(2,10); //O(n)
-        System.out.println(list);
-        System.out.println(list.contains(5)); //O(n)
-        list.add(1,9);
-        System.out.println(list);
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<list.size();i++){
+            if(list.get(i)>max){
+                max=Math.max(list.get(i),max);
+            }
+        }
+        System.out.println("Max element:"+ max);
     } 
 }
