05caab13b5c6103e2dc0637a1507e3fa71192515
Delta Student <125783753+nischithnayak@users.noreply.github.com>
2024-12-01 10:50:29 +0530

    new

--- java/Arraylist/Syntax.java
+++ java/Arraylist/Syntax.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 public class Syntax {
     public static void main(String[] args) {
-        
         ArrayList<Integer> list=new ArrayList<>();
         list.add(1); //O(1)
         list.add(2);
@@ -12,9 +11,10 @@ public class Syntax {
         System.out.println(list.get(2)); //O(1)
         list.remove(2); //O(n)
         System.out.println(list);
-        list.set(2,10);
+        list.set(2,10); //O(n)
         System.out.println(list);
-        System.out.println(list.contains(5));
-    }
-    
+        System.out.println(list.contains(5)); //O(n)
+        list.add(1,9);
+        System.out.println(list);
+    } 
 }
