8ecad8acf9ddb1210c386143fc1a4ed282c21579
Delta Student <125783753+nischithnayak@users.noreply.github.com>
2024-12-10 11:42:34 +0530

    greatest of three

---
+++ java/GreatestofThree.java
@@ -0,0 +1,13 @@
+
+public class GreatestofThree {
+    public static void main(String[] args) {
+        int a=722, b=109,c=37;
+        if(a>b && a>c){
+            System.out.println("A is greatest");    
+        }else if(b>c){
+            System.out.println("B is gratest");
+        }else{
+            System.out.println("c is the greatest" );
+        }
+    }
+}
