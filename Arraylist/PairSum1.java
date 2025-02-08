6d7c66ff66fd8b60027317a167bb3d301a27b34f
Nischith <125783753+nischithnayak@users.noreply.github.com>
2025-02-08 08:02:48 +0530

    new

---
+++ Arraylist/PairSum1.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+public class PairSum1 {
+    //Brute Force
+    public static boolean pairSum1(ArrayList<Integer> list, int target){
+        for(int i=0;i<list.size();i++){
+            for(int j=i+1;i<list.size();j++){
+                if(list.get(i)+list.get(j)==target)
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        //1,2,3,4,5,6
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        int target=50;
+        System.out.println(pairSum1(list, target));
+        
+    }
+}
---
+++ Arraylist/PairSum1Opt.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class PairSum1Opt {
+    public static boolean pairSum1(ArrayList<Integer> list, int target){
+        int lp=0;
+        int rp=list.size()-1;
+        while(lp!=rp){
+            //case1 
+            if(list.get(lp)+list.get(rp)==target){
+                return true;
+            }
+            //case 2
+            if(list.get(lp)+list.get(rp)<target){
+                lp++;
+            }else{
+                //case 3
+                rp--;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+          ArrayList<Integer> list=new ArrayList<>();
+        //1,2,3,4,5,6
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        int target=5;
+        System.out.println(pairSum1(list, target));
+        
+    }
+}
