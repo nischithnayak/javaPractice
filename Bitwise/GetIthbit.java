4b04c14071901a35b9f284f8548ff90ef2f92deb
Nischith <125783753+nischithnayak@users.noreply.github.com>
2025-03-28 12:14:40 +0530

    getIthBit

---
+++ Bitwise/GetIthbit.java
@@ -0,0 +1,13 @@
+public class GetIthbit {
+    public static void main(String[] args) {
+        System.out.println(getIthBit(10, 3));
+    }
+    public static int getIthBit(int n, int i){
+        int bitMask=1<<i;
+        if((n&bitMask)==0){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+}
