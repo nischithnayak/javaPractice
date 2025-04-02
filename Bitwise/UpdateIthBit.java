d9b088f0df8ecd2bf267617451e9b5eb8ae46ee1
Nischith <125783753+nischithnayak@users.noreply.github.com>
2025-04-02 12:02:26 +0530

    UpdateIthBit

---
+++ Bitwise/UpdateIthBit.java
@@ -0,0 +1,12 @@
+public class UpdateIthBit {
+    public static int UpdateIthBit(int b, int i, int newBit){
+        if(newBit==0){
+            return clearBit(int n, int i);
+        }else{
+            return 
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(UpdateIthBit(12,2,1));
+    }
+}
